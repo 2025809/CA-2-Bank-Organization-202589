29c5dbc047f62ca82fb2f38f153623dca3f4e151
2025809 <2025809@student.cct.ie>
2026-05-12 23:23:05 +0100

    Testing

---
+++ src/main/java/com/bankapp/app/bankapp/BankApp.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.bankapp.app.bankapp;
+
+/**
+ *
+ * @author Hamza
+ */
+public class BankApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Hamza!");
+        System.out.println("Hello Hamza!");
+    }
+}
